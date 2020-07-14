9c302ccf88b70a55e0c5f11b4c52542003806094
ruslan <admin94.tm@mail.ru>
2020-07-14 08:51:51 +0300

    After add function night mode

--- js/script.js
+++ js/script.js
@@ -26,4 +26,41 @@ button.element.addEventListener('click', ()=>{
     }
 })
 }
-bindSlideTogle('.hamburger', '[data-slide="nav"]', '.header__menu', 'slide-active')
+bindSlideTogle('.hamburger', '[data-slide="nav"]', '.header__menu', 'slide-active');
+// make swicher function
+let nigth = false;
+const swichMode = () =>{
+    if(nigth === false){
+        document.body.classList.add('night');
+        document.querySelectorAll('.hamburger > line').forEach((item)=>{
+            item.style.stroke='#fff';
+        });
+        document.querySelectorAll('.videos__item-views').forEach((item)=>{
+            item.style.color='#fff'
+        })
+        document.querySelectorAll('.videos__item-descr').forEach((item)=>{
+            item.style.color = '#fff';
+        })
+        document.querySelector('.header__item-descr').style.color = '#fff';
+        document.querySelector('.logo img').src ='logo/youtube_night.svg';
+        nigth=true;
+
+    }else{
+        document.body.classList.remove('night');
+        document.querySelectorAll('.hamburger > line').forEach((item)=>{
+            item.style.stroke='#000';
+        });
+        document.querySelectorAll('.videos__item-views').forEach((item)=>{
+            item.style.color='#000'
+        })
+        document.querySelectorAll('.videos__item-descr').forEach((item)=>{
+            item.style.color = '#000';
+        })
+        document.querySelector('.header__item-descr').style.color = '#000';
+        document.querySelector('.logo img').src ='logo/youtube.svg';
+        nigth=false;
+    }
+}
+swicher.addEventListener('change', ()=>{
+    swichMode();
+})
