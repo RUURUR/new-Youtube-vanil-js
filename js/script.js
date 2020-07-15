962418ad051eae2c1a28fc63d10905142a90abac
ruslan <admin94.tm@mail.ru>
2020-07-15 22:17:27 +0300

    make new function befor work with IFRAME Youtube

--- js/script.js
+++ js/script.js
@@ -148,4 +148,35 @@ bindModal(videos);
        }) 
 }
 
+//close modal
+modal.addEventListener('click', event=>{
+    let target = event.target;
+    event.preventDefault();
+    if(!target.classList.contains('modal__body')){
+        closeModal();
+    }
+})
 
+
+//work with api iframe for show video
+    // 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
+    function onYouTubeIframeAPIReady() {
+      player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: 'M7lc1UVf-VE',
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      });
+    }
